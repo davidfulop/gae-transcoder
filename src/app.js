36d6cf4a6ada1f13b0de571904ed5cb81b223421
David Fulop <david.fulop@7digital.com>
2018-04-06 13:59:42 +0100

    Initial working version.

---
+++ src/app.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START app]
+const express = require('express');
+const fs = require('fs');
+var ffmpeg = require('ffmpeg-stream').ffmpeg;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.setHeader("content-type", "audio/ogg");
+  var converter = ffmpeg();
+
+    var input = converter.input({f: 'flac'});
+    fs.createReadStream(__dirname + "/test01.flac").pipe(input);
+    
+    converter.output({f: 'oga', acodec: 'libvorbis'})
+    .pipe(res);
+     
+    converter.run();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
