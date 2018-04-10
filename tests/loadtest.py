f716b17e83ad824cff294df6ec78c6c675483de2
David Fulop <david.fulop@7digital.com>
2018-04-10 18:13:45 +0100

    Display request times.

--- tests/loadtest.py
+++ tests/loadtest.py
@@ -1,16 +1,20 @@
 import time
 import requests
 import threading
+from datetime import datetime
 
 URL = 'https://transcoder-dot-media-test-1275.appspot.com'
 PROJECT_ID = 'media-test-1275'
-NUM_REQUESTS = 100 
-DELAY=.1
+NUM_REQUESTS = 1
+DELAY=.5
 
 def download_file(i):
-    print("Starting request # " + str(i) + "\n")
+    startTime = datetime.now()
+    print("Starting request # " + str(i) + " at " + str(startTime.time()) + "\n")
     requests.get(URL)
-    print("Finished request # " + str(i) + "\n")
+    endTime = datetime.now()
+    duration = endTime - startTime
+    print("Finished request # " + str(i) + " at " + str(endTime.time()) + " in " + str(duration) + "\n")
 
 for i in range(0, NUM_REQUESTS):
     threading.Thread(target=download_file, args=[i]).start()
